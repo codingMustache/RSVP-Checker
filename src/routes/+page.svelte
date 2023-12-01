43abb494fc7c69c9773855ca363f371467d1f6b6
codingMustache <contact@codingMustache.dev>
2023-12-01 12:09:26 -0600

    some styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,26 +29,31 @@
     display: flex;
     align-items: center;
     justify-content: center;
-    width:100vw;
-    height:100vh;
+    width:400px;
+    height:400px;
+    clip-path: circle(10);
   }
   button{
     width:50%;
     height: 50%;
     color: white;
-    text-shadow: 3px 3px 0px rgb(129, 129, 129);
+    text-shadow: 
+    1px 1px 0px rgb(129, 129, 129),
+    3px 3px 0px rgb(129, 129, 129);
     background-color: rgb(162, 0, 0);
     border-radius: 100%;
     border: unset;
     box-shadow:
     inset 9px 9px 10px rgb(255, 255, 255, 0.5), 
-    9px 9px 10px rgba(0, 0, 0, 0.5), 
+    inset -9px -9px 10px rgb(0, 0, 0, 0.5), 
+    inset -3px -3px 5px rgb(255, 255, 255, 0.5), 
+    9px 9px 10px rgba(0, 0, 0, 0.7), 
     1px 1px 0 red,
     3px 3px 0 red,
     5px 5px 0 red,
     7px 7px 0 red,
     9px 9px 0 red;
-    font-size: 8rem;
+    font-size: 2rem;
     font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
   }
   button:hover{
