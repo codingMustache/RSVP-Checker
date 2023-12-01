35beb743db15140a7ba2c4e21c8b7ba362591722
codingMustache <contact@codingMustache.dev>
2023-12-01 11:54:45 -0600

    made app

---
+++ src/routes/+page.server.js
@@ -0,0 +1,28 @@
+import { WEBHOOK } from '$env/static/private'
+
+
+export const actions = {
+  default: async ()=>{
+
+    const res = await fetch(WEBHOOK, {
+      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      body: JSON.stringify({
+        username: 'Event Bot',
+        avatar_url: "https://static.turbosquid.com/Preview/001180/756/VW/icon-party-3D_0.jpg",
+        embeds: [
+          {
+            color: 11730954,
+            fields: [
+              {
+                name: 'Email',
+                value: "going"
+              }
+            ]
+          }
+        ]
+      })
+
+    })
+  }
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,62 @@
+
+<script>
+  $: clicked = false
+  const onClick = () => clicked = true
+</script>
+
+{#if !clicked}
+<form method="POST">
+  <button on:click={onClick} type="submit">GOING</button>
+</form>
+  {:else}
+  <h1>
+    Thank you!!
+  </h1>
+ {/if}
+
+
+<style>
+  :global(body){
+    margin: 0;
+    width:100vw;
+    height:100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(67, 67, 67);
+  }
+  form{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width:100vw;
+    height:100vh;
+  }
+  button{
+    width:50%;
+    height: 50%;
+    color: white;
+    text-shadow: 3px 3px 0px rgb(129, 129, 129);
+    background-color: rgb(162, 0, 0);
+    border-radius: 100%;
+    border: unset;
+    box-shadow:
+    inset 9px 9px 10px rgb(255, 255, 255, 0.5), 
+    9px 9px 10px rgba(0, 0, 0, 0.5), 
+    1px 1px 0 red,
+    3px 3px 0 red,
+    5px 5px 0 red,
+    7px 7px 0 red,
+    9px 9px 0 red;
+    font-size: 8rem;
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+  button:hover{
+    box-shadow: inset 5px 5px 10px black;
+    scale: .97;
+  }
+  h1{
+    color: white;
+    font-size: xx-large;
+  }
+</style>
